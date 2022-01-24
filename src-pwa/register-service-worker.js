5540b76212bd287b27e027599ba960180a7d436a
Dan Popescu <pdan.popescu@gmail.com>
2022-01-25 00:19:58 +0200

    chore: update favicon, improve pwa loading

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,4 +1,5 @@
 import { register } from 'register-service-worker';
+import { Notify } from 'quasar';
 
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
@@ -29,6 +30,19 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   updated(/* registration */) {
     // console.log('New content is available; please refresh.')
+    Notify.create({
+      message: 'A new version is available.',
+      timeout: 100000,
+      color: 'accent',
+      textColor: 'white',
+      onDismiss() {
+        window.location.reload();
+      },
+      actions: [{
+        label: 'Reload',
+        color: 'white',
+      }],
+    });
   },
 
   offline() {
