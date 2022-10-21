616e0f5b86c747f3320326f9598b4f40aba1ba62
Dan Popescu <pdan.popescu@gmail.com>
2022-10-21 21:43:39 +0300

    fix: conflict markers were wrong for repeated letters

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -389,6 +389,7 @@ export default defineComponent({
                   || (
                     guess.matchTypes[i] !== 'y'
                     && guess.letters.findIndex((l, j) => l === letter && (matchTypes[j] === 'g' || matchTypes[j] === 'y')) > -1
+                    && guess.letters.findIndex((l, j) => l === letter && (guess.matchTypes[j] === 'g' || (j < i && guess.matchTypes[j] === 'y'))) === -1
                   )
                 )
               )) > -1
@@ -400,7 +401,7 @@ export default defineComponent({
               guess.matchTypes[i] === 'g'
               || (
                 guess.matchTypes[i] === 'y'
-                && letters.findIndex((l, j) => j < i && l === letter && (matchTypes[j] === 'g' || matchTypes[j] === 'y')) === -1
+                && letters.findIndex((l, j) => j > i && l === letter && (matchTypes[j] === 'g' || matchTypes[j] === 'y')) === -1
               )
             )
           )) > -1) {
